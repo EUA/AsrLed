cc8165e6384b6e70c33ad7d0968833819d3c0490
Erdem U. Altinyurt <EUA@users.noreply.github.com>
2019-10-19 03:32:54 +0300

    Fixed some functions. setting function automatically sets the mode also.

--- AsrLed.py
+++ AsrLed.py
@@ -19,12 +19,12 @@
 # But probably compatible with all AsRock Motherboards that runs AsrRgbLed utility.
 #
 # If you change RGB values too much, for example using set_static_rgb register in a programatic loop to animate your custom choice,
-# you *PROBABLY* kill your RGB Controller MCU EEPROM, that leads some errors on RGB LED Lightning...
+# you *PROBABLY* kill your RGB Controller MCU EEPROM, that might lead some errors on RGB LED Lightning...
 #
-# Warning: Motherboards that run AsrPolychromeRGB runs nu51_2.10 firmware does NOT compatible with this driver.
+# Warning: Motherboards that run AsrPolychromeRGB runs nu51_2.10 firmware does NOT tested with this driver. Probably not compatible.
 #
 # Register 0x30 controls the device mode. Following RGB code (if exist) and Speed code (if exist). Speed is higher with lower values.
-# I don't understand what the Reg 0x31 for, used by original firmware but things look working prety good without it.
+# I don't understand what the Reg 0x31 for, and 0x32 used by original firmware but things look working pretty good without it.
 #
 
 from smbus import SMBus
@@ -50,9 +50,10 @@ def detect_device():
            if bus.read_byte_data(dev_addr,0x00)==2:
               M=bus.read_byte( dev_addr)
               m=bus.read_byte( dev_addr)
-              if M==1 and m==10:
-                print("Bus:{} Addr:{} Reg:0x00 query returns 1.10.\nPossible AsRock AURA device with FW nu50_1.10 detected.".format(busNum, hex(dev_addr), hex(response)))
-                found=1
+              if (M==1 or M==2) and m==10:
+                print("Bus:{} Addr:{} Reg:0x00 query returns {}.{}.\nPossible AsRock AURA device with FW nu50_{}.{} detected.".format(busNum, hex(dev_addr), M, m, M, m))
+                if M==2:
+                  print("This FW is NOT supported, yet. Try with your own risk!")
                 return bus,dev_addr
      bus.close()
 
@@ -60,7 +61,7 @@ bus,addr= detect_device()
 #bus=SMBus(2)
 #addr=0x6a
 
-def fw_ver(): #just guessing
+def fw_ver():
   bus.read_byte_data( addr, 0x0 )
   M=bus.read_byte( addr ) 
   m=bus.read_byte( addr )
@@ -75,6 +76,16 @@ mode = {'off':0x10,
 	'music':0x17,
 	'wave':0x18}
 
+def dump():
+  for i in range(0,0xff):
+    a=bus.read_byte_data( addr, i )
+    if(a!=0):
+      print( "i:",hex(i),' ',end='')
+      for i in range(a):
+        ret=bus.read_byte(addr)
+        print(hex(ret),end=' ')
+      print('')
+
 def get_mode():
   bus.read_byte_data( addr, 0x30 )
   ret=bus.read_byte(addr)
@@ -83,28 +94,37 @@ def get_mode():
 
 def set_mode( new_mode ):
   bus.write_block_data( addr, 0x30, [mode.get(new_mode)] )
-  #bus.write_block_data( addr, 0x30, [mode] )
+
+def set_off():
+  set_mode('off')
 
 def set_static_rgb(r,g,b):
+  set_mode('static')
   bus.write_block_data( addr, 0x11, [r,g,b] )
 
 def set_breathing_rgbs(r,g,b,s):
+  set_mode('breathing')
   bus.write_block_data( addr, 0x12, [r,g,b,s] )
 
 def set_strobe_rgbs(r,g,b,s):
+  set_mode('strobe')
   bus.write_block_data( addr, 0x13, [r,g,b,s] )
 
 def set_cycling_rgbs(r,g,b,s):
+  set_mode('cycling')
   bus.write_block_data( addr, 0x14, [r,g,b,s] )
 
 def set_random_s(s):
+  set_mode('random')
   bus.write_block_data( addr, 0x15, [s] )
 
 def set_music_rgb(r,g,b):
-  bus.write_byte_data( addr, 0x17, [r,g,b]  )
+  set_mode('music')
+  bus.write_block_data( addr, 0x17, [r,g,b]  )
 
 def set_wave_s(s):
-  bus.write_byte_data( addr, 0x18, s )
+  set_mode('wave')
+  bus.write_block_data( addr, 0x18, [s] )
 
 if __name__ == "__main__":
   print ("Switching Cycling Mode")
